b9fa38d0a9d86a1bd2285d6db646a38894205de3
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2017-10-01 20:48:45 -0400

    Add initial bits for logging

---
+++ logging.cpp
@@ -0,0 +1,75 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2017 Dimitry Ishenko
+// Contact: dimitry (dot) ishenko (at) (gee) mail (dot) com
+//
+// Distributed under the GNU GPL license. See the LICENSE.md file for details.
+
+////////////////////////////////////////////////////////////////////////////////
+#include "logging.hpp"
+
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+
+#include <syslog.h>
+
+////////////////////////////////////////////////////////////////////////////////
+namespace util
+{
+
+////////////////////////////////////////////////////////////////////////////////
+static bool debug_ = false, set_debug_ = true;
+
+bool send_debug() noexcept
+{
+    if(set_debug_)
+    {
+        debug_ = std::getenv("DEBUG");
+        set_debug_ = false;
+    }
+    return debug_;
+}
+
+void send_debug(bool x) noexcept { debug_ = x; set_debug_ = false; }
+
+////////////////////////////////////////////////////////////////////////////////
+static bool console_ = true;
+bool send_to_console() noexcept { return console_; }
+void send_to_console(bool x) noexcept { console_ = x; }
+
+////////////////////////////////////////////////////////////////////////////////
+static bool syslog_ = false;
+bool send_to_syslog() noexcept { return syslog_; }
+void send_to_syslog(bool x) noexcept { syslog_ = x; }
+
+////////////////////////////////////////////////////////////////////////////////
+stream::~stream()
+{
+    std::ostream* os; int pri;
+
+    switch(level_)
+    {
+    case level::debug:
+        if(!send_debug()) return;
+        os = &std::cout; pri = LOG_DEBUG;
+        break;
+
+    case level::info : os = &std::cout; pri = LOG_INFO; break;
+    case level::warn : os = &std::cerr; pri = LOG_WARNING; break;
+    case level::error: os = &std::cerr; pri = LOG_ERR; break;
+
+    default: return;
+    }
+
+    auto s = str();
+    if(send_to_console()) (*os) << s << std::endl;
+    if(send_to_syslog())
+#ifndef _WIN32
+        syslog(pri, "%s", s.data());
+#else
+        throw std::logic_error("Not implemented");
+#endif
+}
+
+////////////////////////////////////////////////////////////////////////////////
+}
---
+++ logging.hpp
@@ -0,0 +1,103 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2017 Dimitry Ishenko
+// Contact: dimitry (dot) ishenko (at) (gee) mail (dot) com
+//
+// Distributed under the GNU GPL license. See the LICENSE.md file for details.
+
+////////////////////////////////////////////////////////////////////////////////
+#ifndef UTIL_LOGGING_HPP
+#define UTIL_LOGGING_HPP
+
+////////////////////////////////////////////////////////////////////////////////
+#include <sstream>
+#include <utility>
+
+////////////////////////////////////////////////////////////////////////////////
+namespace util
+{
+
+////////////////////////////////////////////////////////////////////////////////
+// get/set whether to log debug messages
+//
+// unless explicitly set to false,
+// debug messages will also be logged,
+// if the environment variable DEBUG is set
+//
+bool send_debug() noexcept;
+void send_debug(bool) noexcept;
+
+// get/set whether to log to console
+//
+// if enabled, debug & info messages will be logged to std::cout
+// and warn & error messages will be logged to std::cerr
+//
+bool send_to_console() noexcept;
+void send_to_console(bool) noexcept;
+
+// get/set whether to log to syslog
+//
+bool send_to_syslog() noexcept;
+void send_to_syslog(bool) noexcept;
+
+////////////////////////////////////////////////////////////////////////////////
+// log level
+enum class level { debug, info, warn, error };
+
+////////////////////////////////////////////////////////////////////////////////
+// logging stream
+//
+// instances of this class are returned by
+// debug(), info(), warn() and error() functions;
+//
+class stream : private std::ostringstream
+{
+    using base = std::ostringstream;
+
+public:
+    ////////////////////
+    explicit stream(level l) : level_(l) { }
+    ~stream();
+
+    stream(const stream&) = delete;
+    stream(stream&& rhs) : base(std::move(rhs)), level_(rhs.level_) { }
+
+    stream& operator=(const stream&) = delete;
+    stream& operator=(stream&& rhs)
+    { *static_cast<base*>(this) = std::move(rhs); level_ = rhs.level_; return *this; }
+
+    ////////////////////
+    template<typename T>
+    stream& operator<<(T&& value) &
+    { *static_cast<base*>(this) << std::forward<T>(value); return *this; }
+
+    template<typename T>
+    stream&& operator<<(T&& value) &&
+    { *static_cast<base*>(this) << std::forward<T>(value); return std::move(*this); }
+
+    stream& write(const char* s, std::streamsize n) &
+    { base::write(s, n); return *this; }
+
+    stream&& write(const char* s, std::streamsize n) &&
+    { base::write(s, n); return std::move(*this); }
+
+private:
+    ////////////////////
+    level level_;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// logging functions
+//
+inline auto debug() { return stream(level::debug); }
+inline auto  info() { return stream(level::info ); }
+inline auto  warn() { return stream(level::warn ); }
+inline auto error() { return stream(level::error); }
+
+////////////////////////////////////////////////////////////////////////////////
+}
+
+// pull logging functions into global namespace
+using util::debug; using util::info; using util::warn; using util::error;
+
+////////////////////////////////////////////////////////////////////////////////
+#endif
